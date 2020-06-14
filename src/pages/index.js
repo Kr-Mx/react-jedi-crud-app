d1d26218b6efb6f0e60a5bd2270f25154b515631
Kr-Mx <maks.kryshnev@gmail.com>
2020-06-14 14:41:14 +0300

    fix: resolve problem with routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { Grid, Box } from '@material-ui/core'
 import { People } from './People'
 import { Planets } from './Planets'
 import { Starships } from './Starships'
 import { NoPageFound } from './NoPageFound'
 
 export const PagesRouter = () => (
-  <Switch>
-    <Route path="/people" component={People} />
-    <Route path="/planets" component={Planets} />
-    <Route path="/starships" component={Starships} />
-    <Redirect from="/" to="/people" />
-    <Route path="*" component={NoPageFound} />
-  </Switch>
+  <Box
+    component={Grid}
+    container
+    alignItems="center"
+    justify="center"
+    height="calc(100% - 64px)"
+  >
+    <Switch>
+      <Route exact path="/">
+        <Redirect from="/" to="/people" />
+      </Route>
+      <Route path="/people" component={People} />
+      <Route path="/planets" component={Planets} />
+      <Route path="/starships" component={Starships} />
+      <Route path="*" component={NoPageFound} />
+    </Switch>
+  </Box>
 )
